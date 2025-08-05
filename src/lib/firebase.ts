cbab4d91d0346bf10596a6422b7b3b3f9530c204
Enric Ribas <enric@knomni.com>
2025-08-05 11:11:56 -0400

    Move to .env

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,13 +5,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDXlu3twONtxnlsSYAjJcLng0UsNEgKkkg",
-  authDomain: "fototv-90cf0.firebaseapp.com",
-  projectId: "fototv-90cf0",
-  storageBucket: "fototv-90cf0.firebasestorage.app",
-  messagingSenderId: "341116492373",
-  appId: "1:341116492373:web:74768f1c508198574d12dc",
-  measurementId: "G-K7K2NXMWW3"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
