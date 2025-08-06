df41529e5c95c1de07e443893b5acf32f7772e58
Enric Ribas <enric@knomni.com>
2025-08-06 15:38:56 -0400

    Update version number

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
 	appId: 'com.phototv.app',
-	appName: 'PhotoTV',
+	appName: 'FotoTV',
 	webDir: 'build',
 	server: {
 		androidScheme: 'https'
